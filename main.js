dffbb594219a1a3bdf54b855817702dc99d7e5c3
Dasha <darina1970@mail.ru>
2025-01-19 16:40:19 +0700

    fixed images

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ class Transport {
         return "Price: " + this.price.toLocaleString("en-US", {style:"currency", currency:"USD"});
     }
     getImage() {
-        return this.image.slice(1,-1);
+        return this.image;
     }
 }
 
@@ -49,7 +49,7 @@ const data = [
         brand: 'Audi',
         doors: 4,
         price: 4300000,
-        image: '<https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/2020_Audi_e-Tron_Sport_50_Quattro.jpg/1200px-2020_Audi_e-Tron_Sport_50_Quattro.jpg>'
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/2020_Audi_e-Tron_Sport_50_Quattro.jpg/1200px-2020_Audi_e-Tron_Sport_50_Quattro.jpg'
     },
     {
         id: 2,
@@ -57,7 +57,7 @@ const data = [
         brand: 'Mercedes-Benz',
         doors: 4,
         price: 2800000,
-        image: '<https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/2019_Mercedes-Benz_E220d_SE_Automatic_2.0_Front.jpg/300px-2019_Mercedes-Benz_E220d_SE_Automatic_2.0_Front.jpg>'
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/2019_Mercedes-Benz_E220d_SE_Automatic_2.0_Front.jpg/300px-2019_Mercedes-Benz_E220d_SE_Automatic_2.0_Front.jpg'
     },
     {
         id: 3,
@@ -65,7 +65,7 @@ const data = [
         brand: 'Harley-Davidson',
         maxSpeed: 210,
         price: 1300000,
-        image: '<https://www.harley-davidson.com/content/dam/h-d/images/product-images/bikes/motorcycle/2022/2022-iron-883/2022-iron-883-016/2022-iron-883-016-motorcycle.jpg>'
+        image: 'https://www.harley-davidson.com/content/dam/h-d/images/product-images/bikes/motorcycle/2022/2022-iron-883/2022-iron-883-016/2022-iron-883-016-motorcycle.jpg'
     },
     {
         id: 4,
@@ -73,7 +73,7 @@ const data = [
         brand: 'Harley-Davidson',
         maxSpeed: 220,
         price: 1400000,
-        image: '<https://cdn.dealerspike.com/imglib/products/harley-showroom/2020/livewire/main/Vivid-Black-Main.png>'
+        image: 'https://cdn.dealerspike.com/imglib/products/harley-showroom/2020/livewire/main/Vivid-Black-Main.png'
     }
 ];
 
